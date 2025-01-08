f88796d6a9e7e8b364a1cf511ea472f4d933b13c
Fate <a981211661@gmail.com>
2025-01-08 23:38:06 +0800

    加入專案檔案。

---
+++ CUBETestAPI/Controllers/BitcoinPriceController.cs
@@ -0,0 +1,42 @@
+﻿using CUBETestAPI.Model.ResponseModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CUBETestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BitcoinPriceController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public BitcoinPriceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+        [HttpGet("current")]
+        public async Task<IActionResult> GetBitcoinPrice()
+        {
+            const string coindeskUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(coindeskUrl);
+
+                if(!response.IsSuccessStatusCode)
+                    return StatusCode((int)response.StatusCode, "Failed to fetch data from Coindesk API.");
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<CoindeskResponseModel>(jsonResponse);
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CUBETestAPI/Model/ResponseModel/CoindeskResponseModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace CUBETestAPI.Model.ResponseModel
+{
+    public class CoindeskResponseModel
+    {
+        [JsonProperty("time")]
+        public TimeInfo Time { get; set; }
+        [JsonProperty("bpi")]
+        public BpiInfo Bpi { get; set; }
+
+    }
+    public class TimeInfo
+    {
+        [JsonProperty("updated")]
+        public string Updated { get; set; }
+
+        [JsonProperty("updatedISO")]
+        public string UpdatedISO { get; set; }
+    }
+    public class BpiInfo
+    {
+        [JsonProperty("USD")]
+        public CurrencyInfo USD { get; set; }
+        [JsonProperty("GBP")]
+        public CurrencyInfo GBP { get; set; }
+        [JsonProperty("EUR")]
+        public CurrencyInfo EUR { get; set; }
+    }
+    public class CurrencyInfo
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("rate")]
+        public string Rate { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("rate_float")]
+        public float RateFloat { get; set; }
+    }
+
+}
