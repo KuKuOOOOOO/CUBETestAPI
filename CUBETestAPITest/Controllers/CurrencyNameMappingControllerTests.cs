ac19fd8c0292a8edef9810e57f3581b37daed085
Fate <railgun80851@gmail.com>
2025-01-11 20:24:40 +0800

    <Update> Add unit test 1. Add Controllers Currency CRUD 2. Add Respository Currency CRUD

--- CUBETestAPITest/Controllers/CurrencyNameMappingControllerTests.cs
+++ CUBETestAPITest/Controllers/CurrencyNameMappingControllerTests.cs
@@ -25,12 +25,16 @@ namespace CUBETestAPITest.Controllers
             _controller = new CurrencyNameMappingController(_mockDatabaseService.Object);
         }
 
+        /// <summary>
+        /// Create should return created result
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task CreateCurrencyNameMapping_ShouldReturnCreatedResult_WhenValidDataProvided()
         {
 
             // Arrange
-            var fakeID = Guid.NewGuid();
+            var currencyID = Guid.NewGuid();
             var inputModel = new CurrencyNameMappingInputModel()
             {
                 Currency = "USD",
@@ -39,12 +43,12 @@ namespace CUBETestAPITest.Controllers
 
             var mappedModel = new CurrencyNameMappingModel()
             {
-                ID = fakeID,
+                ID = currencyID,
                 Currency = inputModel.Currency,
                 ChineseName = inputModel.ChineseName
             };
 
-            _mockDatabaseService.Setup(service => service.CreateCurrencyNameMapping(It.IsAny<CurrencyNameMappingModel>())).ReturnsAsync(fakeID);
+            _mockDatabaseService.Setup(service => service.CreateCurrencyNameMapping(It.IsAny<CurrencyNameMappingModel>())).ReturnsAsync(currencyID);
 
             // Act
             var result = await _controller.CreateCurrencyNameMapping(inputModel);
@@ -56,11 +60,118 @@ namespace CUBETestAPITest.Controllers
             actionResult.Should().NotBeNull();
             actionResult.StatusCode.Should().Be(201); // HTTP 201 Created
             actionResult.Value.Should().BeEquivalentTo(mappedModel, options => options.Excluding(m => m.ID));
-            actionResult.RouteValues["id"].Should().Be(fakeID);
+            actionResult.RouteValues["id"].Should().Be(currencyID);
 
             _mockDatabaseService.Verify(service => service.CreateCurrencyNameMapping(It.Is<CurrencyNameMappingModel>(m => m.Currency == inputModel.Currency && 
                                                                                                                           m.ChineseName == inputModel.ChineseName)), Times.Once);
+        }
 
+        /// <summary>
+        /// Get all should return OK
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetAllCurrencyNameMapping_ShouldReturnOK_WhenCurrenciesExist()
+        {
+            // Arrange
+            var currencyNameMappings = new List<CurrencyNameMappingModel>()
+            {
+                new CurrencyNameMappingModel
+                {
+                    ID = Guid.NewGuid(),
+                    Currency = "USD",
+                    ChineseName = "美金"
+                },
+                new CurrencyNameMappingModel
+                {
+                    ID = Guid.NewGuid(),
+                    Currency = "EUR",
+                    ChineseName = "歐元"
+                }
+            };
+
+            _mockDatabaseService.Setup(service => service.GetAllCurrencyNameMapping()).ReturnsAsync(currencyNameMappings);
+
+            // Act
+            var result = await _controller.GetCurrencyNameMapping();
+
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult.StatusCode.Should().Be(200);
+            okResult.Value.Should().BeEquivalentTo(currencyNameMappings);
+        }
+
+        /// <summary>
+        /// Get currency by id should return OK
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetCurrencyNameMappingByID_ShouldReturnOK_WhenCurrencyExist()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+            var currencyNameMapping = new CurrencyNameMappingModel()
+            {
+                ID = currencyID,
+                Currency = "USD",
+                ChineseName = "美金"
+            };
+
+            _mockDatabaseService.Setup(service => service.GetCurrencyNameMapping(currencyID)).ReturnsAsync(currencyNameMapping);
+
+            // Act
+            var result = await _controller.GetCurrencyNameMapping(currencyID);
+
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult.StatusCode.Should().Be(200);
+            okResult.Value.Should().BeEquivalentTo(currencyNameMapping);
+        }
+
+        /// <summary>
+        /// Update Currency should return NoContent
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task UpdateCurrencyNameMapping_ShouldReturnNoContent_WhenCurrencyExist()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+            var currencyNameMapping = new CurrencyNameMappingModel()
+            {
+                ID = currencyID,
+                Currency = "TWD",
+                ChineseName = "新台幣"
+            };
+
+            _mockDatabaseService.Setup(service => service.UpdateCurrencyNameMapping(It.IsAny<CurrencyNameMappingModel>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.UpdateCurrencyNameMapping(currencyID, currencyNameMapping);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+        }
+
+        /// <summary>
+        /// Delete Currency should return NoContent
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task DeleteCurrencyNameMapping_ShouldReturnNoContent_WhenCurrencyExist()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+
+            _mockDatabaseService.Setup(service => service.DeleteCurrencyNameMapping(currencyID)).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.DeleteCurrencyNameMapping(currencyID);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
         }
     }
 }
--- CUBETestAPITest/Respository/CurrencyNameMappingServiceTests.cs
+++ CUBETestAPITest/Respository/CurrencyNameMappingServiceTests.cs
@@ -20,29 +20,138 @@ namespace CUBETestAPITest.Services
             _mockDatabaseService = new Mock<IDatabaseService>();
         }
 
+        /// <summary>
+        /// Create currency should return guid in db
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task CreateCurrencyNameMapping_ShouldReturnGuid_WhenDataIsValid()
         {
 
             // Arrange
-            var fakeID = Guid.NewGuid();
+            var currencyID = Guid.NewGuid();
             var model = new CurrencyNameMappingModel()
             {
-                ID = fakeID,
+                ID = currencyID,
                 Currency = "USD",
                 ChineseName = "美元"
             };
 
-            _mockDatabaseService.Setup(service => service.CreateCurrencyNameMapping(It.Is<CurrencyNameMappingModel>(m => m.ID == fakeID && 
+            _mockDatabaseService.Setup(service => service.CreateCurrencyNameMapping(It.Is<CurrencyNameMappingModel>(m => m.ID == currencyID && 
                                                                                                                          m.Currency == "USD" && 
-                                                                                                                         m.ChineseName == "美元"))).ReturnsAsync(fakeID);
+                                                                                                                         m.ChineseName == "美元"))).ReturnsAsync(currencyID);
 
             // Act
             var result = await _mockDatabaseService.Object.CreateCurrencyNameMapping(model);
 
             //Assert
-            result.Should().Be(fakeID);
+            result.Should().Be(currencyID);
             _mockDatabaseService.Verify(service => service.CreateCurrencyNameMapping(It.IsAny<CurrencyNameMappingModel>()), Times.Once);
+        }
+
+        /// <summary>
+        /// Get all currency should return currency
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetAllCurrencyNameMapping_ShouldReturnCurrencyNameMapping_WhenCurrenciesExist()
+        {
+            // Arrange
+            var currencyNameMappings = new List<CurrencyNameMappingModel>()
+            {
+                new CurrencyNameMappingModel
+                {
+                    ID = Guid.NewGuid(),
+                    Currency = "USD",
+                    ChineseName = "美金"
+                },
+                new CurrencyNameMappingModel
+                {
+                    ID = Guid.NewGuid(),
+                    Currency = "EUR",
+                    ChineseName = "歐元"
+                }
+            };
+
+            _mockDatabaseService.Setup(service => service.GetAllCurrencyNameMapping()).ReturnsAsync(currencyNameMappings);
+
+            // Act
+            var result = await _mockDatabaseService.Object.GetAllCurrencyNameMapping();
+
+            // Assert
+            result.Should().BeEquivalentTo(currencyNameMappings);
+        }
+
+        /// <summary>
+        /// Get currency by ID should return currency
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetCurrencyNameMappingByID_ShouldReturnCurrencyNameMapping_WhenCurrencyExist()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+            var currencyNameMapping = new CurrencyNameMappingModel()
+            {
+                ID = currencyID,
+                Currency = "USD",
+                ChineseName = "美金"
+            };
+
+            _mockDatabaseService.Setup(service => service.GetCurrencyNameMapping(currencyID)).ReturnsAsync(currencyNameMapping);
+
+            // Act
+            var result = await _mockDatabaseService.Object.GetCurrencyNameMapping(currencyID);
+
+            // Assert
+            result.Should().BeEquivalentTo(currencyNameMapping);
+            _mockDatabaseService.Verify(service => service.GetCurrencyNameMapping(currencyID), Times.Once);
+        }
+
+        /// <summary>
+        /// Update currency should return one
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task UpdateCurrencyNameMapping_ShouldReturnOne_WhenUpdateIsSuccessful()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+            var currencyNameMapping = new CurrencyNameMappingModel()
+            {
+                ID = currencyID,
+                Currency = "TWD",
+                ChineseName = "新台幣"
+            };
+
+            _mockDatabaseService.Setup(service => service.UpdateCurrencyNameMapping(It.IsAny<CurrencyNameMappingModel>())).ReturnsAsync(1);
+
+            // Act
+            var result =  await _mockDatabaseService.Object.UpdateCurrencyNameMapping(currencyNameMapping);
+
+            // Assert
+            result.Should().Be(1);
+            _mockDatabaseService.Verify(service => service.UpdateCurrencyNameMapping(currencyNameMapping), Times.Once);
+        }
+
+        /// <summary>
+        /// Delete currency should return one
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task DeleteCurrencyNameMapping_ShouldReturnOne_WhenDeleteIsSuccessful()
+        {
+            // Arrange
+            var currencyID = Guid.NewGuid();
+
+            _mockDatabaseService.Setup(service => service.DeleteCurrencyNameMapping(currencyID)).ReturnsAsync(1);
+
+            // Act
+            var result = await _mockDatabaseService.Object.DeleteCurrencyNameMapping(currencyID);
+
+            // Assert
+            result.Should().Be(1);
+            _mockDatabaseService.Verify(service => service.DeleteCurrencyNameMapping(currencyID), Times.Once);
         }
     }
 }
