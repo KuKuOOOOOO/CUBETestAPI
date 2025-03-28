c9cb6e55bb5275b43d4b754d9ba09d510745dec9
Bubble <bubble_chang@protech.com.tw>
2025-03-28 14:53:15 +0800

    <Update>: Add Encrypt in CurrencyNameMapping
    Co-authored-by: Bubble_chang <bubble_chang@protech.com.tw>

--- CUBETestAPI/Controllers/CurrencyNameMappingController.cs
+++ CUBETestAPI/Controllers/CurrencyNameMappingController.cs
@@ -3,6 +3,7 @@ using CUBETestAPI.Models.ControllerModels;
 using CUBETestAPI.Models.ResponseModels;
 using CUBETestAPI.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,10 +14,12 @@ namespace CUBETestAPI.Controllers
     public class CurrencyNameMappingController : ControllerBase
     {
         private readonly IDatabaseService _databaseService;
+        private readonly RsaCryptoService _rsaService;
 
-        public CurrencyNameMappingController(IDatabaseService databaseService)
+        public CurrencyNameMappingController(IDatabaseService databaseService, RsaCryptoService rsaService)
         {
             _databaseService = databaseService;
+            _rsaService = rsaService;
         }
 
         // GET: api/curreny
@@ -24,7 +27,13 @@ namespace CUBETestAPI.Controllers
         public async Task<ActionResult<IEnumerable<CurrencyNameMappingModel>>> GetCurrencyNameMapping()
         {
             IEnumerable<CurrencyNameMappingModel> currencyNameMappings = await _databaseService.GetAllCurrencyNameMapping();
-            return Ok(currencyNameMappings);
+            var json = JsonConvert.SerializeObject(currencyNameMappings);
+            var encryptedData = _rsaService.Encrypt(json);
+            var data = _rsaService.Decrypt(encryptedData);
+            return Ok(new
+            {
+                Data = data
+            });
 
         }
 
